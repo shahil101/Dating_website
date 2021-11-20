ef6b96d2644bda23094a071dccb90d48c60e15ab
shahil101 <75686817+shahil101@users.noreply.github.com>
2021-11-20 23:45:48 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const counters = document.querySelectorAll('.counter');
+const speed = 100;
+
+counters.forEach(counter => {
+  const updateCount = () => {
+    const target = +counter.getAttribute('data-target');
+    const count = +counter.innerText;
+
+    const inc = target / speed;
+
+    
+
+    if( count < target ){
+      counter.innerText = count + inc;
+      setTimeout(updateCount, 0)
+    }else{
+      count.innerText = target;
+    }
+  }
+  updateCount();
+});
